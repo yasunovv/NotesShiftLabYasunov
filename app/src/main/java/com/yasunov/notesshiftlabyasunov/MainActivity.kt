070e2cc4e346c0261a6d5f27e04466e8ee778bbe
akrilly <yasunovv@ya.ru>
2024-10-15 04:04:12 +1100

    Создал проект с базовым настроенным градлом и hideAppBarAndControls() в MainActivity.kt

---
+++ app/src/main/java/com/yasunov/notesshiftlabyasunov/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.yasunov.notesshiftlabyasunov
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        hideAppBarAndControls()
+        setContent {
+
+        }
+    }
+
+    private fun hideAppBarAndControls() {
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView).apply {
+                systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+
+        window.decorView.setOnApplyWindowInsetsListener { view, windowInsets ->
+            windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
+            view.onApplyWindowInsets(windowInsets)
+        }
+
+    }
+
+}
